ba3fd632c48e66c18340b23340996c1f56a87713
Félix Ernesto Charry Pastrana <feecharrypa@unal.edu.co>
2021-07-15 13:27:22 -0500

    blend two imgs

--- face_recognition.py
+++ face_recognition.py
@@ -3,7 +3,8 @@ import face_recognition
 import glob
 
 # All files ending with .txt
-files = glob.glob("faces/*.JPG")
+
+files = glob.glob("faces/*.jpeg")
 for file in files:
     image = face_recognition.load_image_file(file)
     face_locations = face_recognition.face_locations(image)
@@ -19,3 +20,28 @@ for file in files:
         face_image = image[top:bottom, left:right]
         pil_image = Image.fromarray(face_image)
         pil_image.save(file[:-4]+'_cut.jpg')
+
+
+imfg = Image.open(r"faces/ATXME6008_cut.jpg").convert("RGBA")
+
+imbg = Image.open(r"faces/DSC_0040_cut.jpg").convert("RGBA")
+
+imbg_width, imbg_height = imbg.size
+imfg_resized = imfg.resize((imbg_width, imbg_height), Image.LANCZOS)
+
+#Test = Image.alpha_composite(imfg_resized, imbg_width)
+#Test.save("test.png", format="png")
+
+imbg = Image.open(r"faces/profile_smile._cut.jpg").convert("RGBA")
+blended = Image.blend(imbg, imbg, alpha=1)
+imbg_width, imbg_height = imbg.size
+
+files = glob.glob("faces/*_cut.jpg")
+for file in files:
+    imfg = Image.open(file).convert("RGBA")
+    imfg_resized = imfg.resize((imbg_width, imbg_height), Image.LANCZOS)
+    blended = Image.blend(imbg, imbg, alpha=1)
+
+blended = Image.blend(imfg_resized, imbg, alpha=1)
+blended.save("blended.png")
+
